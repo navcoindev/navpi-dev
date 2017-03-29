15331dbc0a5b2de5ce2ce935fcfe620976714dc6
Craig <craig.b.macgregor@gmail.com>
2017-03-30 12:50:27 +1300

    uncommited updates

--- NavCoinlockstate.php
+++ NavCoinlockstate.php
@@ -1,3 +1,3 @@
 <?php
-$lockState='Not Encrypted';
+$lockState='Locked';
 ?>
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<?php 
-include ("header.php"); 
-include ("pass.php"); 
-$info = $coin->getinfo(); 
+<?php
+include ("header.php");
+include ("pass.php");
+$info = $coin->getinfo();
 $y = array_reverse($info);
-$bal1 = $coin->getbalance(); 
-$bal2 = $coin->getbalance("*", 0); 
-$bal3 = abs($bal1 - $bal2); 
+$bal1 = $coin->getbalance();
+$bal2 = $coin->getbalance("*", 0);
+$bal3 = abs($bal1 - $bal2);
 $bal4 = abs("{$y['stake']}");
 if($primary!=""){
 	$address = $primary;
@@ -15,42 +15,42 @@ else{
 }
 
 if ($currentWallet == NavCoin){
-	$stakinginfo = $coin->getstakinginfo(); 
-	$x = array_reverse($stakinginfo); 
-	$time = $x['expectedtime']; 
+	$stakinginfo = $coin->getstakinginfo();
+	$x = array_reverse($stakinginfo);
+	$time = $x['expectedtime'];
 }
 
-	$days = floor($time / 86400); 
-	$hours = floor(($time / 3600) % 24); 
-	$minutes = floor(($time / 60) % 60); 
-	$fiatValue = ($bal1 * $price * $fiatBTC); 
-	$fiatValue = sprintf("%01.2f", $fiatValue); 
-	$btcValue = ($bal1 * $price); 
-	$btcValue = sprintf("%01.8f", $btcValue); 
-	$img = shell_exec("qrencode --output=- -l H -d 144 -s 50 -m 1 $address"); $imgData = "data:image/png;base64," . base64_encode($img); 
+	$days = floor($time / 86400);
+	$hours = floor(($time / 3600) % 24);
+	$minutes = floor(($time / 60) % 60);
+	$fiatValue = ($bal1 * $price * $fiatBTC);
+	$fiatValue = sprintf("%01.2f", $fiatValue);
+	$btcValue = ($bal1 * $price);
+	$btcValue = sprintf("%01.8f", $btcValue);
+	$img = shell_exec("qrencode --output=- -l H -d 144 -s 50 -m 1 $address"); $imgData = "data:image/png;base64," . base64_encode($img);
 ?>
 <div class="row">
 	<div class="col-lg-6">
 		<h3>Available Balance: <font color='green'><?php echo $bal1; ?></font> <?php echo $currentWallet; ?></h3>
-		
+
 		<h4>Unavailable Due To Staking: <font color='red'><?php echo $bal4; ?></font> <?php echo $currentWallet; ?></h4>
 		<h4>BTC Value: <font color='green'><?php echo "{$btcValue}"; ?></font></h4>
 		<h4><?php echo $longCurrency; ?> Value: <font color='green'><?php echo "{$symbol}{$fiatValue}"; ?></font></h4><br>
 		<div class="col-lg-8">
-                	<form action="lockcontrol">
-                   		<button class='btn btn-default btn-block'>Your Wallet Is <?php print($lockState)?> Click To Change</button>
-                	</form>	
+    	<form action="lockcontrol">
+       		<button class='btn btn-default btn-block'>Your Wallet Is <?php print($lockState)?> Click To Change</button>
+    	</form>
 		</div>
 	</div>
 	<div class="col-lg-3">
 		<?php if (isset($_POST['show'])){
-			$privKey = $coin->dumpprivkey($address); 
-			$privKeyImg = shell_exec("qrencode --output=- -l H -d 144 -s 50 -m 1 $privKey"); 
-			$privKeyImgData = "data:image/png;base64," . base64_encode($privKeyImg); 
+			$privKey = $coin->dumpprivkey($address);
+			$privKeyImg = shell_exec("qrencode --output=- -l H -d 144 -s 50 -m 1 $privKey");
+			$privKeyImgData = "data:image/png;base64," . base64_encode($privKeyImg);
 		?>
 		<center><img class="emrQRCode" src="<?=$privKeyImgData?>"height="30%" />
 		<h4>Private Key</h4></center>
-		<?php 
+		<?php
 		}
 		else {
 		?>
@@ -59,7 +59,7 @@ if ($currentWallet == NavCoin){
 				<button class='btn btn-default' type="submit" name="show" value="show">Show private key</button>
 			</form>
 		<h4>wallet must be unlocked for sending, or not encrypted.</h4></center>
-		<?php 
+		<?php
 		}
 		?>
 	</div>
@@ -72,7 +72,7 @@ if ($currentWallet == NavCoin){
 <div class="well">
 <div class="row">
 	<div class="col-lg-7">
-	<p> Your main wallet address is <?php print_r($address); ?>.    
+	<p> Your main wallet address is <?php print_r($address); ?>.
 	<p>The network is currently on block <?php print_r($coin->getblockcount()); ?>.
 	<?php if ($currentWallet == NavCoin): ?>
 		<?php echo "<p>Your stake weight is {$w['combined']}{$sw['combined']}{$x['weight']}{$x['Weight']}.</p>" ?>
@@ -98,14 +98,14 @@ if ($currentWallet == NavCoin){
 		<a href='update' class='btn btn-default' role='button'>A new update to the WebUI is available, click to get it.</a>
 	<?php endif; ?>
 	</div>
-     <?php if(file_exists("/home/stakebox/UI/".$currentWallet."notes.php")){ 
-       include('/home/stakebox/UI/'.$currentWallet.'notes.php'); 
+     <?php if(file_exists("/home/stakebox/UI/".$currentWallet."notes.php")){
+       include('/home/stakebox/UI/'.$currentWallet.'notes.php');
 	echo "<div class='col-lg-5'>
 	  <div class='form-group'>
 	  <form action='notes' method='POST'><input type='hidden'>
 	    <label for='notes'>Notes:</label>
 	      <textarea class='form-control' name='notes' id='notes' cols='60' rows='10'>$notes</textarea>
-		<button class='btn btn-default' type='submit' value='setprimary'>Save Notes</button>
+		<button class='btn btn-default' type='submit' value='setprimary' style='margin-top:10px;'>Save Notes</button>
 	  </form>
 	  </div>
 	</div>";}
@@ -115,7 +115,7 @@ if ($currentWallet == NavCoin){
 	  <form action='notes' method='POST'><input type='hidden'>
 	    <label for='notes'>Notes:</label>
 	      <textarea class='form-control' name='notes' id='notes' cols='60' rows='10'></textarea>
-		<button class='btn btn-default' type='submit' value='setprimary'>Save Notes</button>
+		<button class='btn btn-default' type='submit' value='setprimary' style='margin-top:10px;'>Save Notes</button>
 	  </form>
 	  </div>
 	</div>";}
