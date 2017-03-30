1ecb6e8e831d9ff7d54fb13c5df7898a4e718b38
Craig <craig.b.macgregor@gmail.com>
2017-03-31 09:51:49 +1300

    private payment added to sendtoaddress

--- sendtoaddress.php
+++ sendtoaddress.php
@@ -27,6 +27,10 @@ $bal1 = $coin->getbalance();
                     <td><input class="form-control" type="text" name="comment" value="<?php print($comment); ?>"></td>
                 </tr>
                 <tr>
+                    <td><b>Private payment</b></td>
+                    <td><input class="form-control" type="checkbox" name="anonsend" value="1" <?php if($_POST['anonsend'] == "1"){ echo "checked";} ?>/></td>
+                </tr>
+                <tr>
                     <td>&nbsp;</td>
                     <td><button class='btn btn-blue' type="submit" value="Preview"><b>Send Coins</b></button></td>
 		</tr>
